021dbe48768a3c3fd6df250b1227d1eae1388bf1
drwnd <markus.vonhaenisch@gmail.com>
2025-10-11 11:42:06 +0200

    Updated assets/structures to match Structure.java format

---
+++ src/main/java/game/server/saving/StructureSaver.java
@@ -0,0 +1,31 @@
+package game.server.saving;
+
+import game.server.MaterialsData;
+import game.server.generation.Structure;
+
+public final class StructureSaver extends Saver<Structure> {
+    @Override
+    void save(Structure structure) {
+        saveInt(structure.sizeX());
+        saveInt(structure.sizeY());
+        saveInt(structure.sizeZ());
+        saveInt(structure.materials().getTotalSizeBits());
+        saveByteArray(structure.materials().getBytes());
+    }
+
+    @Override
+    Structure load() {
+        int sizeX = loadInt();
+        int sizeY = loadInt();
+        int sizeZ = loadInt();
+        int totalSizeBits = loadInt();
+        byte[] data = loadByteArray();
+
+        return new Structure(sizeX, sizeY, sizeZ, new MaterialsData(totalSizeBits, data));
+    }
+
+    @Override
+    Structure getDefault() {
+        return null;
+    }
+}
