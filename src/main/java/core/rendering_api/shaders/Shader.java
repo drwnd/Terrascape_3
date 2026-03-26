d00f21afc483bbe0e01ace8424aef0d01694cd91
drwnd <markus.vonhaenisch@gmail.com>
2026-03-26 19:05:22 +0100

    Added some uniform setters In Shader.java

--- src/main/java/core/rendering_api/shaders/Shader.java
+++ src/main/java/core/rendering_api/shaders/Shader.java
@@ -22,77 +22,88 @@ public abstract class Shader implements Asset {
 
     public void setUniform(String uniformName, Matrix4f value) {
         try (MemoryStack stack = MemoryStack.stackPush()) {
-            glUniformMatrix4fv(uniforms.get(uniformName), false, value.get(stack.mallocFloat(16)));
+            glUniformMatrix4fv(getUniform(uniformName), false, value.get(stack.mallocFloat(16)));
         }
     }
 
     public void setUniform(String uniformName, int[] data) {
-        glUniform1iv(uniforms.get(uniformName), data);
+        glUniform1iv(getUniform(uniformName), data);
     }
 
     public void setUniform(String uniformName, float[] data) {
-        glUniform1fv(uniforms.get(uniformName), data);
-    }
-
-    public void setUniform(String uniformName, int x, int y) {
-        glUniform2i(uniforms.get(uniformName), x, y);
-    }
-
-    public void setUniform(String uniformName, Vector2i value) {
-        glUniform2i(uniforms.get(uniformName), value.x, value.y);
-    }
-
-    public void setUniform(String uniformName, Vector3i value) {
-        glUniform3i(uniforms.get(uniformName), value.x, value.y, value.z);
-    }
-
-    public void setUniform(String uniformName, int x, int y, int z) {
-        glUniform3i(uniforms.get(uniformName), x, y, z);
-    }
-
-    public void setUniform(String uniformName, long x, long y, long z) {
-        glUniform3i(uniforms.get(uniformName), (int) x, (int) y, (int) z);
-    }
-
-    public void setUniform(String uniformName, int x, int y, int z, int w) {
-        glUniform4i(uniforms.get(uniformName), x, y, z, w);
-    }
-
-    public void setUniform(String uniformName, float x, float y, float z) {
-        glUniform3f(uniforms.get(uniformName), x, y, z);
-    }
-
-    public void setUniform(String uniformName, float x, float y, float z, float w) {
-        glUniform4f(uniforms.get(uniformName), x, y, z, w);
-    }
-
-    public void setUniform(String uniformName, float value) {
-        glUniform1f(uniforms.get(uniformName), value);
-    }
-
-    public void setUniform(String uniformName, Vector2f value) {
-        glUniform2f(uniforms.get(uniformName), value.x, value.y);
-    }
-
-    public void setUniform(String uniformName, float x, float y) {
-        glUniform2f(uniforms.get(uniformName), x, y);
-    }
-
-    public void setUniform(String uniformName, Vector3f value) {
-        glUniform3f(uniforms.get(uniformName), value.x, value.y, value.z);
-    }
-
-    public void setUniform(String uniformName, int value) {
-        glUniform1i(uniforms.get(uniformName), value);
-    }
-
-    public void setUniform(String uniformName, boolean value) {
-        glUniform1i(uniforms.get(uniformName), value ? 1 : 0);
+        glUniform1fv(getUniform(uniformName), data);
     }
 
     public void setUniform(String uniformName, Color color) {
-        glUniform3f(uniforms.get(uniformName), color.getRed() * 0.003921569F, color.getGreen() * 0.003921569F, color.getBlue() * 0.003921569F);
+        glUniform3f(getUniform(uniformName), color.getRed() * 0.003921569F, color.getGreen() * 0.003921569F, color.getBlue() * 0.003921569F);
     }
+
+    public void setUniform(String uniformName, boolean value) {
+        glUniform1i(getUniform(uniformName), value ? 1 : 0);
+    }
+
+
+    public void setUniform(String uniformName, int value) {
+        glUniform1i(getUniform(uniformName), value);
+    }
+
+    public void setUniform(String uniformName, int x, int y) {
+        glUniform2i(getUniform(uniformName), x, y);
+    }
+
+    public void setUniform(String uniformName, int x, int y, int z) {
+        glUniform3i(getUniform(uniformName), x, y, z);
+    }
+
+    public void setUniform(String uniformName, int x, int y, int z, int w) {
+        glUniform4i(getUniform(uniformName), x, y, z, w);
+    }
+
+    public void setUniform(String uniformName, Vector2i value) {
+        glUniform2i(getUniform(uniformName), value.x, value.y);
+    }
+
+    public void setUniform(String uniformName, Vector3i value) {
+        glUniform3i(getUniform(uniformName), value.x, value.y, value.z);
+    }
+
+    public void setUniform(String uniformName, Vector4i value) {
+        glUniform4i(getUniform(uniformName), value.x, value.y, value.z, value.w);
+    }
+
+    public void setUniform(String uniformName, long x, long y, long z) {
+        glUniform3i(getUniform(uniformName), (int) x, (int) y, (int) z);
+    }
+
+
+    public void setUniform(String uniformName, float value) {
+        glUniform1f(getUniform(uniformName), value);
+    }
+
+    public void setUniform(String uniformName, float x, float y) {
+        glUniform2f(getUniform(uniformName), x, y);
+    }
+
+    public void setUniform(String uniformName, float x, float y, float z) {
+        glUniform3f(getUniform(uniformName), x, y, z);
+    }
+
+    public void setUniform(String uniformName, float x, float y, float z, float w) {
+        glUniform4f(getUniform(uniformName), x, y, z, w);
+    }
+
+    public void setUniform(String uniformName, Vector2f value) {
+        glUniform2f(getUniform(uniformName), value.x, value.y);
+    }
+
+    public void setUniform(String uniformName, Vector3f value) {
+        glUniform3f(getUniform(uniformName), value.x, value.y, value.z);
+    }
+
+    public void setUniform(String uniformName, Vector4f value) {
+        glUniform4f(getUniform(uniformName), value.x, value.y, value.z, value.w);
+    }
+
 
     @Override
     public void delete() {
@@ -139,6 +150,11 @@ public abstract class Shader implements Asset {
         if (uniformLocation == -1) System.err.println("Could not find uniform " + uniformName);
         uniforms.put(uniformName, uniformLocation);
         System.out.printf("-Created uniform %s with binding %s%n", uniformName, uniformLocation);
+    }
+
+    private int getUniform(String uniformName) {
+        Integer location = uniforms.get(uniformName);
+        return location == null ? -1 : location;
     }
 
     private static String strip(String line) {
