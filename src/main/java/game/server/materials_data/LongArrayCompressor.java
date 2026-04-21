13e498b90f338ffd6f4d67ee9043a670cb11f329
drwnd <markus.vonhaenisch@gmail.com>
2026-04-21 08:41:21 +0200

    testing more direct homogeneity computation

--- src/main/java/game/server/materials_data/LongArrayCompressor.java
+++ src/main/java/game/server/materials_data/LongArrayCompressor.java
@@ -18,8 +18,8 @@ final class LongArrayCompressor {
     }
 
     private static int compressMaterials(ByteArrayList data, ByteBuffer uncompressedMaterials, int sizeBits, int startIndex, int inChunkX, int inChunkY, int inChunkZ) {
-        int homogeneity = getHomogeneity(inChunkX, inChunkY, inChunkZ, sizeBits, uncompressedMaterials);
-        if (homogeneity == FULLY_HOMOGENOUS) return addHomogenous(data, uncompressedMaterials, inChunkX, inChunkY, inChunkZ);
+        if (isHomogenous(MaterialsData.getUncompressedIndex(inChunkX, inChunkY, inChunkZ), 1 << sizeBits * 3, uncompressedMaterials))
+            return addHomogenous(data, uncompressedMaterials, inChunkX, inChunkY, inChunkZ);
         if (sizeBits <= 1) {
             long materials = uncompressedMaterials.getLong(getUncompressedIndex(inChunkX, inChunkY, inChunkZ));
             data.add((byte) (getTypes(materials) | DETAIL));
@@ -39,77 +39,24 @@ final class LongArrayCompressor {
         data.add(SPLITTER);
         data.pad(SPLITTER_BYTE_SIZE - 1);
 
-        if ((homogeneity & 1) == 0)
-            offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX, inChunkY, inChunkZ);
-        else offset += addHomogenous(data, uncompressedMaterials, inChunkX, inChunkY, inChunkZ);
+        offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX, inChunkY, inChunkZ);
         setOffset(data, offset, startIndex + 1);
-
-        if ((homogeneity & 2) == 0)
-            offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX, inChunkY, inChunkZ + nextSize);
-        else offset += addHomogenous(data, uncompressedMaterials, inChunkX, inChunkY, inChunkZ + nextSize);
+        offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX, inChunkY, inChunkZ + nextSize);
         setOffset(data, offset, startIndex + 4);
-
-        if ((homogeneity & 4) == 0)
-            offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX, inChunkY + nextSize, inChunkZ);
-        else offset += addHomogenous(data, uncompressedMaterials, inChunkX, inChunkY + nextSize, inChunkZ);
+        offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX, inChunkY + nextSize, inChunkZ);
         setOffset(data, offset, startIndex + 7);
-
-        if ((homogeneity & 8) == 0)
-            offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX, inChunkY + nextSize, inChunkZ + nextSize);
-        else offset += addHomogenous(data, uncompressedMaterials, inChunkX, inChunkY + nextSize, inChunkZ + nextSize);
+        offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX, inChunkY + nextSize, inChunkZ + nextSize);
         setOffset(data, offset, startIndex + 10);
-
-        if ((homogeneity & 16) == 0)
-            offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX + nextSize, inChunkY, inChunkZ);
-        else offset += addHomogenous(data, uncompressedMaterials, inChunkX + nextSize, inChunkY, inChunkZ);
+        offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX + nextSize, inChunkY, inChunkZ);
         setOffset(data, offset, startIndex + 13);
-
-        if ((homogeneity & 32) == 0)
-            offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX + nextSize, inChunkY, inChunkZ + nextSize);
-        else offset += addHomogenous(data, uncompressedMaterials, inChunkX + nextSize, inChunkY, inChunkZ + nextSize);
+        offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX + nextSize, inChunkY, inChunkZ + nextSize);
         setOffset(data, offset, startIndex + 16);
-
-        if ((homogeneity & 64) == 0)
-            offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX + nextSize, inChunkY + nextSize, inChunkZ);
-        else offset += addHomogenous(data, uncompressedMaterials, inChunkX + nextSize, inChunkY + nextSize, inChunkZ);
+        offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX + nextSize, inChunkY + nextSize, inChunkZ);
         setOffset(data, offset, startIndex + 19);
+        offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX + nextSize, inChunkY + nextSize, inChunkZ + nextSize);
 
-        if ((homogeneity & 128) == 0)
-            offset += compressMaterials(data, uncompressedMaterials, sizeBits, startIndex + offset, inChunkX + nextSize, inChunkY + nextSize, inChunkZ + nextSize);
-        else offset += addHomogenous(data, uncompressedMaterials, inChunkX + nextSize, inChunkY + nextSize, inChunkZ + nextSize);
         data.set((byte) (getSplitterTypes(data, index) | SPLITTER), index);
         return offset;
-    }
-
-    private static int getHomogeneity(int startX, int startY, int startZ, int sizeBits, ByteBuffer uncompressedMaterials) {
-        int startIndex = getUncompressedIndex(startX, startY, startZ);
-        if (sizeBits == 1) return isHomogenous(startIndex, 8, uncompressedMaterials) ? FULLY_HOMOGENOUS : 0;
-
-        int homogeneity = 0;
-        int length = (1 << --sizeBits * 3);
-
-        if (isHomogenous(startIndex, length, uncompressedMaterials)) homogeneity |= 1;
-        if (isHomogenous(startIndex += length, length, uncompressedMaterials)) homogeneity |= 2;
-        if (isHomogenous(startIndex += length, length, uncompressedMaterials)) homogeneity |= 4;
-        if (isHomogenous(startIndex += length, length, uncompressedMaterials)) homogeneity |= 8;
-        if (isHomogenous(startIndex += length, length, uncompressedMaterials)) homogeneity |= 16;
-        if (isHomogenous(startIndex += length, length, uncompressedMaterials)) homogeneity |= 32;
-        if (isHomogenous(startIndex += length, length, uncompressedMaterials)) homogeneity |= 64;
-        if (isHomogenous(startIndex + length, length, uncompressedMaterials)) homogeneity |= 128;
-
-        if (homogeneity == 255) {
-            int size = 1 << sizeBits * 3;
-            startIndex = getUncompressedIndex(startX, startY, startZ);
-            long materials = uncompressedMaterials.getLong(startIndex);
-            if (uncompressedMaterials.getLong(startIndex += size) == materials
-                    && uncompressedMaterials.getLong(startIndex += size) == materials
-                    && uncompressedMaterials.getLong(startIndex += size) == materials
-                    && uncompressedMaterials.getLong(startIndex += size) == materials
-                    && uncompressedMaterials.getLong(startIndex += size) == materials
-                    && uncompressedMaterials.getLong(startIndex += size) == materials
-                    && uncompressedMaterials.getLong(startIndex + size) == materials) return FULLY_HOMOGENOUS;
-        }
-        return homogeneity;
     }
 
     private static boolean isHomogenous(int startIndex, int length, ByteBuffer uncompressedMaterials) {
